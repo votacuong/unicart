4b55d4ddd3bad17806a874a0479ecec979e4a3c0
Võ Tá Cương <162108993+votacuong@users.noreply.github.com>
2026-04-15 06:26:21 +0700

    delete

--- app/Views/front-end/cart/fail.php
+++
@@ -1,1 +0,0 @@
-<?php VLang::__e('SYSTEM_FAIL');?>
--- public/front-end/js/home.js
+++
@@ -1,8 +0,0 @@
-function addToCart(product_id)
-{
-	jQuery.get(addCart+product_id, function(data){
-		jQuery.get(countCart, function(data){
-			jQuery('#cart-count').html(data);
-		});
-	});
-}
